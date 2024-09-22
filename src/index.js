432c7844e6c7b27aa6e1eac3ff537bf7c8b5a10e
שמואל <kamanzisamuel3.com>
2024-09-21 20:30:15 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
+const express  = require('express');
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
 const {v4:uuidv4} = require('uuid');
-const id = uuidv4();
-console.log("your unique id is:",id);
+const data = uuidv4();
+const PORT =9000;
+
+const customers = [
+    {
+"name":"samuel",
+"company":"Afritech ltd"
+
+
+    },
+    {
+
+"name":"samuel",
+"company":"Afritech ltd"
+
+    },
+    {
+"name":"samuel",
+"company":"Afritech ltd"
+
+    }
+]
+
+app.get('/',(req,res)=>{
+    res.send("welcome!");
+})
+
+
+app.get('/api/customers',(req,res)=>{
+    res.send({"customers":customers})
+
+})
+
+
+app.get('/unique',(req,res)=>{
+
+    res.send(data);
+})
+
+
+
+app.post('/customer/api',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
+
+app.post('/ip/headers',(req,res)=>{
+    console.log(req.headers)
+    res.send(req.headers)
+})
+
+
+
+
+app.listen(PORT,()=>{
+    console.log("server is listening on port of :",PORT);
+})
+
+console.log("your unique id is :",data);
+
---
+++ src/used.js
@@ -0,0 +1,18 @@
+// const {v4:uuidv4} = require('uuid');
+// const id = uuidv4();
+// console.log("your unique id is:",id);
+
+
+
+// const http = require('http');
+
+
+// server=http.createServer((req,res)=>{
+//     res.statusCode=200;
+//     res.setHeader('contentType','text/plain');
+//     res.end("hello");
+// })
+
+// server.listen('5000',()=>{
+//     console.log("server is running");
+// })
